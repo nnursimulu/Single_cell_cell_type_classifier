15ea96e3bc7314e3619abfec30850b9f99c24650
nnursimulu <anavrin.ulumisrun@gmail.com>
2023-08-11 19:06:27 -0400

    kNN cv code

--- scripts/classifier_cv/kNN.py
+++ scripts/classifier_cv/kNN.py
@@ -1,59 +1,58 @@
+import utils
 import os
-import scipy
-import numpy as np
 from argparse import ArgumentParser
-from sklearn.model_selection import StratifiedKFold
-import pickle
-
-
+from sklearn.neighbors import KNeighborsClassifier
 
 if __name__ == '__main__':
     
-    parser = ArgumentParser(description="Splits the scRNA count matrix and corresponding labels into training," + \
-        "validation and test data set.")
-    parser.add_argument("--input_folder", type=str, help="Folder containing class_labels.in and count_matrix.in.",
+    parser = ArgumentParser(description="Cross-validation of k-nearest neighbour for different values of k.")
+    parser.add_argument("--training_folder", type=str, help="Absolute path to folder that contains the training data.",
                         required=True)
-    parser.add_argument("--output_folder", type=str, help="Folder to contain split data.",
+    parser.add_argument("--results_folder", type=str, help="Absolute path to folder that will contain the results",
                         required=True)
 
     args = parser.parse_args()
-    input_folder = args.input_folder
-    output_folder = args.output_folder
+    training_folder = args.training_folder
+    results_folder = args.results_folder
 
-    # Read feature vectors and labels.  Text labels will be associated with IDs for easier manipulation via sklearn.
-    count_matrix = scipy.io.mmread(input_folder + "/count_matrix.in").tocsr()
-    feature_vectors = convert_to_fvs(count_matrix)
-    ordered_ids, id_to_label = read_labels(input_folder + "/class_labels.in")
-    
-    # Write out the number of cells in each category
-    print ("========Distribution of raw data:")
-    print_freq_ids(ordered_ids, id_to_label)
+    # Create the results directory if it does not exist yet.
+    if not os.path.exists(results_folder):
+        os.makedirs(results_folder)
 
-    # Split into train and test set.  Have training be 80% and test 20% of the data.
-    # Set a seed for reproducibility.
-    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=999)
-    train_indices, test_indices = next(skf.split(feature_vectors, ordered_ids))
-    train_fvs, train_label_ids = get_fvs_from_indices(feature_vectors, ordered_ids, train_indices)
-    test_fvs, test_label_ids = get_fvs_from_indices(feature_vectors, ordered_ids, test_indices)
+    # Values of k to try.
+    k_values = [5, 10, 25, 50, 75, 100]
 
-    # Create the output directory if it does not exist yet.
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+    # Dict with accuracy values for different values of k and sections of the data.
+    dict_k_to_section_to_accuracy = {}
 
-    # Write out the train and test feature vectors (as pickled data structures).
-    # Also pickle the ID to label dictionary for easy access when learning.
-    pickle_fvs_and_labels(train_fvs, train_label_ids, "OVERALL_training", output_folder)
-    pickle_fvs_and_labels(test_fvs, test_label_ids, "OVERALL_test", output_folder)
-    pickle_object(id_to_label, output_folder + "/id_to_label.pkl")
+    print ("========Running kNN cross-validation for different values of k.")
 
-    # Split training data for 5-fold cross-validation.
-    skf_cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=999)
-    for i, (train_indices_cv, test_indices_cv) in enumerate(skf_cv.split(train_fvs, train_label_ids)):
-        train_fvs_cv, train_label_ids_cv = get_fvs_from_indices(train_fvs, train_label_ids, train_indices_cv)
-        test_fvs_cv, test_label_ids_cv = get_fvs_from_indices(train_fvs, train_label_ids, test_indices_cv)
+    # For each subsection of the data, try different values of k and see impact on accuracy.
+    fold_i = 0
+    while fold_i < 5:
+        # Load the training feature vectors and corresponding labels for this round of cross-validation.
+        prefix_file = training_folder + "/CV_" + str(fold_i)
+        training_fvs = utils.unpickle_object(prefix_file + "_training_fvs.pkl")
+        training_fvs = utils.convert_to_dense_repr(training_fvs)
+        training_label_IDs = utils.unpickle_object(prefix_file + "_training_label_IDs.pkl")
 
-        # Also write out train and test feature vectors, again as pickled data structures.
-        pickle_fvs_and_labels(train_fvs_cv, train_label_ids_cv, "CV_" + str(i) + "_training", output_folder)
-        pickle_fvs_and_labels(test_fvs_cv, test_label_ids_cv, "CV_" + str(i) + "_test", output_folder)
+        # Load the test feature vectors and corresponding labels for this round of cross-validation.
+        test_fvs = utils.unpickle_object(prefix_file + "_test_fvs.pkl")
+        test_fvs = utils.convert_to_dense_repr(test_fvs)
+        test_label_IDs = utils.unpickle_object(prefix_file + "_test_label_IDs.pkl")
 
-    print ("===========Data splitting done.")
+        # Try different value of k for the kNN classifier.
+        for k_value in k_values:
+            print ("======Training for k=" + str(k_value) + ", section of the data:" + str(fold_i))
+            knn = KNeighborsClassifier(n_neighbors=k_value)
+            knn.fit(training_fvs, training_label_IDs)
+
+            predicted_label_IDs = knn.predict(test_fvs)
+            accuracy = utils.compute_accuracy(predicted_label_IDs, test_label_IDs)
+            
+            utils.update_accuracy_dict(dict_k_to_section_to_accuracy, str(k_value), fold_i, accuracy)
+        fold_i += 1
+
+    # Summarize the value of the accuracy across different sections of the data, and write out results.
+    dict_k_to_accuracy = utils.summarize_accuracy_across_hyperparameters(dict_k_to_section_to_accuracy)
+    utils.write_out_summarized_accuracy(dict_k_to_accuracy, results_folder + "/CV_knn.out")
---
+++ scripts/classifier_cv/utils.py
@@ -0,0 +1,103 @@
+import pickle
+
+def write_out_summarized_accuracy(dict_setting_to_accuracy, output_file):
+    """Write out summarized accuracy across different settings to output file
+    
+    Parameters:
+    dict_setting_to_accuracy: dict mapping hyperparameter setting (str) to averaged accuracy across all sections of data (float)
+    output_file: str denoting absolute path to output file
+    """
+
+    with open(output_file, "w") as writer:
+        writer.write("Setting\tAccuracy\n")
+        for setting, accuracy in dict_setting_to_accuracy.items():
+            writer.write(setting + "\t" + str(accuracy) + "\n")
+
+
+def update_accuracy_dict(dict_setting_to_section_to_accuracy, setting, fold_i, accuracy):
+    """Update dictionary of accuracy with accuracy for this particular setting (set of hyperparameters) and 
+    this section of the data.
+    
+    Parameters:
+    dict_setting_to_section_to_accuracy: dict mapping setting of hyperparameters to section of data to accuracy.
+    setting: A string representing the particular combination of hyperparameters used.
+    fold_i: an int, denoting a different section of the training data for cross-validation.
+    accuracy: float, computed accuracy for this section of the data and this set of hyperparameters.
+    """
+
+    if setting not in dict_setting_to_section_to_accuracy:
+        dict_setting_to_section_to_accuracy[setting] = {}
+    dict_setting_to_section_to_accuracy[setting][fold_i] = accuracy
+
+
+def summarize_accuracy_across_hyperparameters(dict_setting_to_section_to_accuracy):
+    """Return dict with summarize accuracy across different hyperparameter setting.
+    
+    Parameters:
+    dict_setting_to_section_to_accuracy: dict mapping hyperparameter setting (str) to section of data (int) to accuracy (float).
+
+    Returns:
+    dict_setting_to_accuracy: dict mapping hyperparameter setting (str) to averaged accuracy across all sections of data (float)
+    """
+
+    dict_setting_to_accuracy = {}
+    for setting, section_to_accuracy in dict_setting_to_section_to_accuracy.items():
+        accuracy_array = []
+        for section, accuracy in section_to_accuracy.items():
+            accuracy_array.append(accuracy)
+        avg_accuracy = sum(accuracy_array)/len(accuracy_array)
+        dict_setting_to_accuracy[setting] = avg_accuracy
+    return dict_setting_to_accuracy
+
+
+def unpickle_object(filename):
+    """Unpickle and return object.
+    
+    Parameter:
+    filename: str denoting absolute path to an object.
+    
+    Return:
+    Unpickled object.
+    """
+
+    with open(filename, 'rb') as input_data:
+        var = pickle.load(input_data)
+    return var
+
+
+def compute_accuracy(predicted_IDs, actual_IDs):
+    """Compute overall accuracy based on predicted and actual labels.
+    Accuracy is computed as (number of correct predictions)/(total number of labels)
+    
+    Parameter:
+    predicted_IDs: list of int (IDs) indicating predicted labels of feature vectors.
+    actual_IDs: list of int (IDs) indicating actual labels of feature vectors. 
+
+    Return:
+    A float representing accuracy.
+    """
+
+    num_correct = 0
+    for pred_ID, actual_ID in zip(predicted_IDs, actual_IDs):
+        if pred_ID == actual_ID:
+            num_correct += 1
+    accuracy = float(num_correct)/len(actual_IDs)
+    return accuracy
+
+
+def convert_to_dense_repr(sparse_feature_vectors):
+    """
+    Convert an array of sparse feature vectors to an array of array.
+
+    Parameter:
+    sparse_feature_vectors: array of sparse feature vectors (matrix objects)
+
+    Return:
+    An array of arrays, each representing a feature vector.
+    """
+
+    dense_feature_vectors = []
+    for fv in sparse_feature_vectors:
+        dense_fv = fv.toarray().tolist()[0]
+        dense_feature_vectors.append(dense_fv)
+    return dense_feature_vectors
